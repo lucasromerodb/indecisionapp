853fde9c0b0b11f895255d8dadc094af8ad834ae
Lucas Romero Di Benedetto <lro@3818.com.ar>
2018-02-07 13:40:52 -0300

    visibilidad de un elemento

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,97 +1,39 @@
 'use strict';
 
-var app = {
-  title: 'Indecision App',
-  subtitle: 'Pon tu vida en las manos de la tecnología',
-  options: []
-};
-
-var onFormSubmit = function onFormSubmit(e) {
-  e.preventDefault();
-  var optionValue = e.target.elements.option.value;
-  if (optionValue) {
-    app.options.push(optionValue);
-    e.target.elements.option.value = '';
-    renderApp();
-  }
-};
-
-var removeItems = function removeItems() {
-  app.options = [];
-  renderApp();
-};
-
-var onMakeDecision = function onMakeDecision() {
-  var randomNum = Math.floor(Math.random() * app.options.length);
-  var option = app.options[randomNum];
-  console.log(option);
-};
-
+var visibility = false;
 var appRoot = document.getElementById('app');
 
-var renderApp = function renderApp() {
+var toggleVisibility = function toggleVisibility() {
+  visibility = !visibility;
+  render();
+};
 
+var render = function render() {
   var template = React.createElement(
     'div',
-    { id: 'myp' },
+    null,
     React.createElement(
-      'h2',
+      'h1',
       null,
-      app.title
-    ),
-    app.subtitle && React.createElement(
-      'p',
-      null,
-      app.subtitle
-    ),
-    React.createElement(
-      'p',
-      null,
-      app.options.length > 0 ? 'Here are your options ' : 'No options'
-    ),
-    React.createElement(
-      'ol',
-      null,
-      app.options.map(function (option) {
-        return React.createElement(
-          'li',
-          { key: option },
-          option
-        );
-      })
+      'Visibility Toggle'
     ),
     React.createElement(
       'button',
-      { disabled: app.options.length === 0, onClick: onMakeDecision },
-      'What should i do?'
+      { onClick: toggleVisibility },
+      visibility ? 'Hide details' : 'Show details'
     ),
-    React.createElement('br', null),
-    React.createElement('br', null),
-    React.createElement(
-      'form',
-      { onSubmit: onFormSubmit },
-      React.createElement('input', { type: 'text', name: 'option' }),
+    visibility && React.createElement(
+      'div',
+      null,
       React.createElement(
-        'button',
+        'p',
         null,
-        'Add option'
+        'Hey, these are some details you can now see!'
       )
-    ),
-    React.createElement('br', null),
-    React.createElement(
-      'button',
-      { onClick: removeItems },
-      'Remove all'
-    ),
-    React.createElement('hr', null),
-    React.createElement(
-      'small',
-      null,
-      'babel src/app.js --out-file=scripts/app.js --presets=env,react --watch'
     )
   );
 
   ReactDOM.render(template, appRoot);
 };
 
-renderApp();
+render();
