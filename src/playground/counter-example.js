1e81edfe37ab1bb0961a96af97f147cc4ae5a20a
Lucas Romero Di Benedetto <lro@3818.com.ar>
2018-02-09 11:33:00 -0300

    estado de componentes

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,22 +1,77 @@
-let count = 0;
+class Counter extends React.Component {
 
-const addOne = () => {count++; console.log(count); renderCounterApp();}
-const minusOne = () => {count--; console.log(count); renderCounterApp();}
-const reset = () => {count = 0; console.log(count); renderCounterApp();}
+  constructor(props){
+    super(props);
+    this.handleAddOne = this.handleAddOne.bind(this);
+    this.handleMinusOne = this.handleMinusOne.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.state = {
+      count: 0,
+    }
+  }
 
-const appRoot =  document.getElementById('app');
+  handleAddOne() {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1
+      }
+    })
 
-const renderCounterApp = () => {
-  const like = (
-    <div>
-      <h1>Count: {count}</h1>
-      <button onClick={addOne}>+1</button>
-      <button onClick={minusOne}>-1</button>
-      <button onClick={reset}>Reset</button>
-    </div>
-  )
+  }
+  handleMinusOne() {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count - 1
+      }
+    })
+  }
+  handleReset() {
+    this.setState(() => {
+      return {
+        count: 0
+      }
+    });
 
-  ReactDOM.render(like, appRoot);
-};
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1
+      }
+    })
+  }
 
-renderCounterApp();
+  render() {
+    return(
+      <div>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.handleMinusOne}>-1</button>
+        <button onClick={this.handleAddOne}>+1</button>
+        <button onClick={this.handleReset}>Reset</button>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Counter />, document.getElementById('app'));
+
+// let count = 0;
+//
+// const addOne = () => {count++; console.log(count); renderCounterApp();}
+// const minusOne = () => {count--; console.log(count); renderCounterApp();}
+// const reset = () => {count = 0; console.log(count); renderCounterApp();}
+//
+// const appRoot =  document.getElementById('app');
+//
+// const renderCounterApp = () => {
+//   const like = (
+//     <div>
+//       <h1>Count: {count}</h1>
+//       <button onClick={addOne}>+1</button>
+//       <button onClick={minusOne}>-1</button>
+//       <button onClick={reset}>Reset</button>
+//     </div>
+//   )
+//
+//   ReactDOM.render(like, appRoot);
+// };
+//
+// renderCounterApp();
--- src/playground/show-info.js
+++ src/playground/show-info.js
@@ -1,27 +1,35 @@
-let visibility = false;
-const appRoot = document.getElementById('app');
+class VisibilityToggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+    this.state = {
+      visibility: false
+    }
+  }
 
-const toggleVisibility = () => {
-  visibility = !visibility;
-  render();
+  handleToggleVisibility() {
+    this.setState((prevState) => {
+      return {
+        visibility: !prevState.visibility
+      }
+    })
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Visibility Toggle</h1>
+        <button onClick={this.handleToggleVisibility}>
+          { this.state.visibility ? 'Hide details' : 'Show details' }
+        </button>
+        {this.state.visibility && (
+          <div>
+            <p>Hey, these are some details you can now see!</p>
+          </div>
+        )}
+      </div>
+    )
+  }
 }
 
-const render = () => {
-  const template = (
-    <div>
-      <h1>Visibility Toggle</h1>
-      <button onClick={toggleVisibility}>
-        { visibility ? 'Hide details' : 'Show details' }
-      </button>
-      {visibility && (
-        <div>
-          <p>Hey, these are some details you can now see!</p>
-        </div>
-      )}
-    </div>
-  )
-
-  ReactDOM.render(template, appRoot);
-}
-
-render();
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
