980c9a73409a6ddfded1a6b53f269fae24079ac5
Lucas Romero Di Benedetto <lro@3818.com.ar>
2018-02-16 16:58:13 -0300

    setear y consumir datos en local storage para mostrar información en los ciclos de un componente react como componentDidMount y componentDidUpdate

--- indecision-app/src/index.js
+++ indecision-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './App';
+// import Counter from './Counter';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('root'));
